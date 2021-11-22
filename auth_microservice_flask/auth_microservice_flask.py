ccf5fa655adf77e33e6d9c46df2b521d64be69da
khalil-farashiani <khalilfarashi2000@gmail.com>
2021-11-22 19:36:13 +0330

    adding database configuration

--- auth_microservice_flask/auth_microservice_flask.py
+++ auth_microservice_flask/auth_microservice_flask.py
@@ -1,7 +1,12 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from auth_microservice_flask.config import Config
+
+db = SQLAlchemy()
+mg = Migrate()
 
 apiv1_bp = Blueprint("apiv1", __name__, url_prefix='/api/v1/')
 apiv1 = Api(apiv1_bp)
@@ -11,5 +16,8 @@ from auth_microservice_flask import resource
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config) #load application configs 
+    db.init_app(app) #initial SQLAlchemy
+    mg.init_app(app, db) #initial migration
     app.register_blueprint(apiv1_bp) #load API v1 
+    
     return app 
--- auth_microservice_flask/config/config.py
+++ auth_microservice_flask/config/config.py
@@ -3,8 +3,20 @@ from os import environ
 class Config:
 
     # ================== Global Configuration=====================
+
     ENV = environ.get("AUTHMAN_ENV", "production")
 
     TESTING = int(environ.get("AUTHMAN_TESTING", "0"))
 
     DEBUG = int(environ.get("AUTHMAN_DEBUG", "0"))
+
+
+    # ================== Database Configuration=====================
+
+    SQLALCHEMY_DATABASE_URI = environ.get("AUTHMAN_DATABASEURI", None)
+
+    SQLALCHEMY_ECHO = DEBUG
+
+    SQLALCHEMY_RECORF_QUERIES = DEBUG
+
+    SQLALCHEMY_TRACK_MODIFICATION = DEBUG
