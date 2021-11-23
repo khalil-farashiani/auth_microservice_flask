99daded16c635b0aa296ddad3c2dd485900da12d
khalil-farashiani <khalilfarashi2000@gmail.com>
2021-11-23 20:29:57 +0330

    adding uuidgen function

--- auth_microservice_flask/util/__init__.py
+++ auth_microservice_flask/util/__init__.py
@@ -1,1 +1,2 @@
 from auth_microservice_flask.util.jsonify import jsonify
+from auth_microservice_flask.util.uuidgen import uuidgen
---
+++ auth_microservice_flask/util/uuidgen.py
@@ -0,0 +1,4 @@
+from uuid import uuid4
+
+def uuidgen():
+    return uuid4.hex()
